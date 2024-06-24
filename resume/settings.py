03789407db22cec264f5ec0b1b6451b1094efafb
hernanlozano93 <hdlozanor@udistrital.edu.co>
2024-06-24 18:06:58 -0500

    Commit 10

--- resume/settings.py
+++ resume/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'core/static/'
 STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = "/home/ubuntu/Resume/core/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
